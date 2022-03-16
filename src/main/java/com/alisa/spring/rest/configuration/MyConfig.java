87c3882f5f8c7ed6efe7c8639da303f2bfc0a863
Alisa Stepanova <alisa.ve@mail.ru>
2022-03-16 22:41:56 +0300

    Initial commit

---
+++ src/main/java/com/alisa/spring/rest/configuration/MyConfig.java
@@ -0,0 +1,45 @@
+package com.alisa.spring.rest.configuration;
+
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import jdk.vm.ci.meta.Local;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+
+@Configuration
+@ComponentScan(basePackages = "com.alisa.spring.rest")
+@EnableWebMvc
+public class MyConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false");
+            dataSource.setUser("bestuser");
+            dataSource.setPassword("bestuser");
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        return dataSource;
+
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.alisa.spring.rest.entity");
+
+
+        return sessionFactory;
+    }
+
+}
